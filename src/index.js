c7e19f0d8f7deae0a30c9f8c27560e3a22f32937
Siavash Namvar <snsinahub@github.com>
2022-09-20 12:00:45 -0400

    use function

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ const github = require('@actions/github');
 const core = require('@actions/core');
 const fs = require('fs');
 const os = require('os');
+const filePath = process.env[`GITHUB_ENV`]
 
-
+function add_env_variable(name, val) {
+    fs.appendFileSync(filePath, `${name}=${val}${os.EOL}`, {
+        encoding: 'utf8'
+    })
+}
 
 async function run() {
     const jsonPath = core.getInput('json_path')
-    const filePath = process.env[`GITHUB_ENV`]
-
-    fs.appendFileSync(filePath, `FULL_NAME=JOHN DOE${os.EOL}`, {
-        encoding: 'utf8'
-    })
+    
+    add_env_variable(FULL_NAME, 'JOHN DOE')
+    add_env_variable(STATE, 'Virginia')
+    add_env_variable(CITY, 'Arlington')
+    // fs.appendFileSync(filePath, `FULL_NAME=JOHN DOE${os.EOL}`, {
+    //     encoding: 'utf8'
+    // })
 
     core.setOutput("envs", 'snsinas')
 
